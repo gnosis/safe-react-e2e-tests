737e42d292cc0660d656c12b9fbed5a4bca2b070
unknown <juampibermani@gmail.com>
2021-07-05 12:31:05 -0300

    Make the test generic

--- src/modify_policies.test.js
+++ src/modify_policies.test.js
@@ -48,7 +48,7 @@ describe('Change Policies', () => {
       await isTextPresent('body', 'Contract Version', gnosisPage)
       await clickByText('p', 'Policies', gnosisPage)
       await isTextPresent('body', 'Required confirmations', gnosisPage)
-      await isTextPresent('body', '2 out of 4 owners', gnosisPage)
+      await isTextPresent('body', '2 out of', gnosisPage)
       await clickByText('span', 'Change', gnosisPage)
       await isTextPresent('body', 'Change required confirmations', gnosisPage)
       // Creating and approving Tx with owner 1
@@ -74,7 +74,6 @@ describe('Change Policies', () => {
       // Verifying the change in the settings
       await gnosisPage.bringToFront()
       await gnosisPage.waitForTimeout(2000)
-      console.log('Checking for the 2 out of 2')
       await assertTextPresent(transactionsTab.tx_status, 'Pending', gnosisPage, 'css')
       // waiting for the queue list to be empty and the executed tx to be on the history tab
       await assertElementPresent(transactionsTab.no_tx_in_queue, gnosisPage, 'css')
@@ -86,15 +85,12 @@ describe('Change Policies', () => {
       await clickElement(transactionsTab.tx_type, gnosisPage)
       const changeConfirmationText = await gFunc.getInnerText('div.tx-details > p', gnosisPage, 'css')
       expect(changeConfirmationText).toBe('Change required confirmations:')
-      const txDetails = await gFunc.getInnerText('div.tx-details', gnosisPage, 'css')
-      console.log(txDetails)
-      console.log('Confirmed the 2 out of 2')
       await isTextPresent(generalInterface.sidebar, 'SETTINGS', gnosisPage)
       await clickByText('span', 'SETTINGS', gnosisPage)
       await isTextPresent('body', 'Contract Version', gnosisPage)
       await clickByText('p', 'Policies', gnosisPage)
       await isTextPresent('body', 'Required confirmations', gnosisPage)
-      await isTextPresent('body', '1 out of 4 owners', gnosisPage)
+      await isTextPresent('body', '1 out of', gnosisPage)
       // Create Tx to revert it back to initial state
       await clickByText('span', 'Change', gnosisPage)
       await isTextPresent('body', 'Change required confirmations', gnosisPage)
@@ -113,7 +109,6 @@ describe('Change Policies', () => {
       // Wating for the tx execution notification, history should update after
       await isTextPresent('body', 'Transaction successfully executed', gnosisPage)
       const expectedNonce = transactionNonce + 1
-      console.log('Waiting for tx with nonce: ', expectedNonce)
       await gnosisPage.waitForTimeout(1000)
       nonce = await getNumberInString(transactionsTab.tx_nonce, gnosisPage, 'css')
       expect(nonce).toBe(expectedNonce)
@@ -122,7 +117,7 @@ describe('Change Policies', () => {
       await isTextPresent('body', 'Contract Version', gnosisPage)
       await clickByText('p', 'Policies', gnosisPage)
       await isTextPresent('body', 'Required confirmations', gnosisPage)
-      await isTextPresent('body', '2 out of 4 owners', gnosisPage)
+      await isTextPresent('body', '2 out of', gnosisPage)
       done()
     } catch (error) {
       done(error)
