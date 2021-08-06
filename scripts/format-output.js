1904b1f65ceb557bbcf41a9210b1ee006a144779
unknown <juampibermani@gmail.com>
2021-08-06 12:49:39 -0300

    format output script

--- scripts/format-output.js
+++ scripts/format-output.js
@@ -1,7 +1,16 @@
 const output = require('../output.json')
 
 /*const errors = output.testResults.map((testResults) => {
-    testResults.ma
+    testResults.map((result)=>{
+        if(result.failureMessages.length > 0){
+            console.log(`Test: ${result.title}`)
+            result.failureMessages.map((messages)=> {
+                console.log(messages)
+            })
+        }
+    })
+    console.log(JSON.stringify(results))
 })*/
+
 
 console.log('List of errors')
