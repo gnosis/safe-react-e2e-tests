45d03733d0c89821aa9d924ab01cb30e69bdcbe6
unknown <juampibermani@gmail.com>
2021-08-11 12:06:03 -0300

    Right format for e2e tests comments

--- scripts/format-output.js
+++ scripts/format-output.js
@@ -1,14 +1,22 @@
 const output = require('../output.json')
 
-console.log("\n")
-console.log('Failed tests:')
+
+const errorsText = []
 for (let i = 0; i < output.testResults.length; i++) {
     const testResult = output.testResults[i];
     for (let j = 0; j < testResult.assertionResults.length; j++) {
         const testCase = testResult.assertionResults[j];
         if(testCase.failureMessages.length > 0){
-            console.log("\n")
-            console.log(testCase.fullName)
+            errorsText.push("\\n")
+            errorsText.push(JSON.stringify(` - ❌ ${testCase.fullName}`).slice(1, -1))
         }
     }
+}
+
+if(errorsText.length > 0) {
+  console.log("\\n")
+  console.log('Failed tests:')
+  errorsText.forEach((text) => {
+    console.log(text)
+  })
 }
