ff71efdb368fb140b0f0ae619f29aa4e3723e2f6
unknown <juampibermani@gmail.com>
2021-09-03 13:00:39 -0300

    Update script to describe tests

--- scripts/describe-tests.js
+++ scripts/describe-tests.js
@@ -6,12 +6,22 @@ const directoryPath = path.join(__dirname, '/../src')
 const fileNames = fs.readdirSync(directoryPath, 'utf8')
 
 
+function addStr(str, index, stringToAdd){
+  return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
+}
+
 fileNames.forEach((fileName)=>{
   const data = fs.readFileSync(path.join(directoryPath, fileName), 'utf8')
-  const comment = data.substring(
+  let comment = data.substring(
     data.indexOf("/*") + 2, 
     data.lastIndexOf("*/")
   );
+  let lineCount = 1
+  while (comment.indexOf('--') > 0) {
+    comment = addStr(comment, comment.indexOf('--')-2, `\\\n${lineCount}.`)
+    comment = comment.replace('--', '')
+    lineCount ++
+  }
   console.log('#### ' + fileName)
   console.log(comment)
 })
