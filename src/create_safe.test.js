70a0095993c1de8f6bc8927da9c7c36f5010c793
Franco Venica <francovenica@gmail.com>
2021-09-13 14:02:20 -0300

    "adding local_storage test and localStgConstants"

--- src/create_safe.test.js
+++ src/create_safe.test.js
@@ -49,49 +49,44 @@ describe('Create New Safe', () => {
   test('Create Safe', async () => {
     console.log('Open Create Safe Form\n')
     // Open Create Safe Form
-    try {
-      await clickByText('p', '+ Create New Safe', gnosisPage)
-      await assertElementPresent(createSafePage.form, gnosisPage, 'css')
-      // Naming The Safe
-      await clickAndType({ selector: createSafePage.safe_name_field, type: 'css' }, gnosisPage, newSafeName)
-      await clickElement(generalInterface.submit_btn, gnosisPage)
-      // Adding Owners
-      await assertElementPresent(createSafePage.step_two, gnosisPage, 'css')
-      await clickElement({ selector: createSafePage.add_owner_btn }, gnosisPage) // adding new row
-      await gnosisPage.waitForTimeout(1000)
-      await clickElement(generalInterface.submit_btn, gnosisPage) // making "Required error" show up for the new owner fields
-      await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // check first "required" error in name field
-      await clickAndType({ selector: createSafePage.owner_name_field(1), type: 'css' }, gnosisPage, owner2Name) // filling name field
-      await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // checking "required" error in address field
-      await clickAndType({ selector: createSafePage.address_field(1), type: 'css' }, gnosisPage, owner2Address)
-      await assertElementPresent(createSafePage.valid_address(1), gnosisPage, 'css')
-      rowsAmount = await gnosisPage.$$eval(createSafePage.owner_row, (x) => x.length) // see how many owner I've created
-      await assertElementPresent(createSafePage.req_conf_limit(rowsAmount), gnosisPage, 'css') // that amount should be in the text "out of X owners"
-      // Setting Required Confirmation
-      await clickElement({ selector: createSafePage.threshold_select_input }, gnosisPage)
-      await clickElement({ selector: createSafePage.select_input(rowsAmount) }, gnosisPage)
-      await gnosisPage.waitForTimeout(2000) // gotta wait before clickin review_btn or doesn't work
-      await clickElement(generalInterface.submit_btn, gnosisPage)
-      // Reviewing Safe Info
-      await assertElementPresent(createSafePage.step_three, gnosisPage, 'css')
-      await assertTextPresent(createSafePage.review_safe_name, newSafeName, gnosisPage, 'css')
-      await assertElementPresent(createSafePage.review_req_conf(rowsAmount), gnosisPage, 'css')
-      await assertTextPresent(createSafePage.review_owner_name(1), owner2Name, gnosisPage, 'css')
-      await assertTextPresent(createSafePage.review_owner_address(1), owner2Address, gnosisPage, 'css')
-      await clickElement(generalInterface.submit_btn, gnosisPage)
-      await MMpage.bringToFront()
-      await MMpage.waitForTimeout(2000)
-      await metamask.confirmTransaction()
-      // Assert Safe Creation
-      await gnosisPage.bringToFront()
-      await assertElementPresent(createSafePage.back_btn, gnosisPage, 'css')
-      await assertElementPresent(createSafePage.etherscan_link, gnosisPage, 'css')
-      await assertElementPresent(createSafePage.continue_btn, gnosisPage, 'css')
-      await clickElement({ selector: createSafePage.continue_btn }, gnosisPage)
-      await isTextPresent(generalInterface.sidebar, accountsSelectors.safeNames.create_safe_name, gnosisPage)
-      done()
-    } catch (error) {
-      done(error)
-    }
+    await clickByText('p', '+ Create New Safe', gnosisPage)
+    await assertElementPresent(createSafePage.form, gnosisPage, 'css')
+    // Naming The Safe
+    await clickAndType({ selector: createSafePage.safe_name_field, type: 'css' }, gnosisPage, newSafeName)
+    await clickElement(generalInterface.submit_btn, gnosisPage)
+    // Adding Owners
+    await assertElementPresent(createSafePage.step_two, gnosisPage, 'css')
+    await clickElement({ selector: createSafePage.add_owner_btn }, gnosisPage) // adding new row
+    await gnosisPage.waitForTimeout(1000)
+    await clickElement(generalInterface.submit_btn, gnosisPage) // making "Required error" show up for the new owner fields
+    await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // check first "required" error in name field
+    await clickAndType({ selector: createSafePage.owner_name_field(1), type: 'css' }, gnosisPage, owner2Name) // filling name field
+    await assertElementPresent(errorMsg.error(errorMsg.required), gnosisPage) // checking "required" error in address field
+    await clickAndType({ selector: createSafePage.address_field(1), type: 'css' }, gnosisPage, owner2Address)
+    await assertElementPresent(createSafePage.valid_address(1), gnosisPage, 'css')
+    rowsAmount = await gnosisPage.$$eval(createSafePage.owner_row, (x) => x.length) // see how many owner I've created
+    await assertElementPresent(createSafePage.req_conf_limit(rowsAmount), gnosisPage, 'css') // that amount should be in the text "out of X owners"
+    // Setting Required Confirmation
+    await clickElement({ selector: createSafePage.threshold_select_input }, gnosisPage)
+    await clickElement({ selector: createSafePage.select_input(rowsAmount) }, gnosisPage)
+    await gnosisPage.waitForTimeout(2000) // gotta wait before clickin review_btn or doesn't work
+    await clickElement(generalInterface.submit_btn, gnosisPage)
+    // Reviewing Safe Info
+    await assertElementPresent(createSafePage.step_three, gnosisPage, 'css')
+    await assertTextPresent(createSafePage.review_safe_name, newSafeName, gnosisPage, 'css')
+    await assertElementPresent(createSafePage.review_req_conf(rowsAmount), gnosisPage, 'css')
+    await assertTextPresent(createSafePage.review_owner_name(1), owner2Name, gnosisPage, 'css')
+    await assertTextPresent(createSafePage.review_owner_address(1), owner2Address, gnosisPage, 'css')
+    await clickElement(generalInterface.submit_btn, gnosisPage)
+    await MMpage.bringToFront()
+    await MMpage.waitForTimeout(2000)
+    await metamask.confirmTransaction()
+    // Assert Safe Creation
+    await gnosisPage.bringToFront()
+    await assertElementPresent(createSafePage.back_btn, gnosisPage, 'css')
+    await assertElementPresent(createSafePage.etherscan_link, gnosisPage, 'css')
+    await assertElementPresent(createSafePage.continue_btn, gnosisPage, 'css')
+    await clickElement({ selector: createSafePage.continue_btn }, gnosisPage)
+    await isTextPresent(generalInterface.sidebar, accountsSelectors.safeNames.create_safe_name, gnosisPage)
   }, 180000)
 })
---
+++ utils/files/LocalStorageConstants.js
@@ -0,0 +1,406 @@
+export const ADDRESSBOOK_KEY = "SAFE__addressBook"
+export const ADDRESSBOOK_VALUES = [
+    {
+        "address": "0x9913B9180C20C6b0F21B6480c84422F6ebc4B808",
+        "name": "Safe1",
+        "chainId": 4
+    },
+    {
+        "address": "0x612DcD86cfa8AeA16C3B3f9E204694B51466F0F6",
+        "name": "Safe2",
+        "chainId": 4
+    },
+    {
+        "address": "0x5BC79B27731589B43c51f745315ca899b4056f33",
+        "name": "Safe3",
+        "chainId": 4
+    },
+    {
+        "address": "0x00Be14E33760FCD61e46C619eC089a3BfB6d1352",
+        "name": "safe4",
+        "chainId": 4
+    },
+    {
+        "address": "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+        "name": "User1",
+        "chainId": 4
+    },
+    {
+        "address": "0x7724b234c9099C205F03b458944942bcEBA13408",
+        "name": "user2",
+        "chainId": 4
+    },
+    {
+        "address": "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B",
+        "name": "User3",
+        "chainId": 4
+    },
+    {
+        "address": "0x730F87dA2A3C6721e2196DFB990759e9bdfc5083",
+        "name": "User4",
+        "chainId": 4
+    }
+]
+export const SAFES_KEY = "_immortal|v2_RINKEBY__SAFES"
+export const SAFES_VALUES = {
+    "0x9913B9180C20C6b0F21B6480c84422F6ebc4B808":
+    {
+        "address": "0x9913B9180C20C6b0F21B6480c84422F6ebc4B808",
+        "threshold": 2,
+        "ethBalance": "16.659799999999892",
+        "totalFiatBalance": "503062.05",
+        "owners": [
+            "0x61BDdFA8600acfaE37eEf6D7Be0Dd85745389906",
+            "0x69904ff6d6100799344E5C9A2806936318F6ba4f",
+            "0x66bE167c36B3b75D1130BBbDec69f9f04E7DA4fC",
+            "0x6484a2514AEE516DdaC6f67Dd2322f23e0A4A7D6",
+            "0x8c35B7eE520277D14af5F6098835A584C337311b",
+            "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B",
+            "0x11B1D54B66e5e226D6f89069c21A569A22D98cfd",
+            "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F",
+            "0xDe75665F3BE46D696e5579628fA17b662e6fC04e",
+            "0x6f965E48347AF3Df65c14CCc176A9CbeCEa0eDb5",
+            "0x730F87dA2A3C6721e2196DFB990759e9bdfc5083",
+            "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+            "0x7724b234c9099C205F03b458944942bcEBA13408"
+        ],
+        "modules": null,
+        "spendingLimits": [
+            {
+                "delegate": "0x730F87dA2A3C6721e2196DFB990759e9bdfc5083",
+                "token": "0x0000000000000000000000000000000000000000",
+                "amount": "2000000000000000000",
+                "spent": "0",
+                "resetTimeMin": "30",
+                "lastResetMin": "27124997",
+                "nonce": "1"
+            },
+            {
+                "delegate": "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B",
+                "token": "0x0000000000000000000000000000000000000000",
+                "amount": "1000000000000000000",
+                "spent": "0",
+                "resetTimeMin": "10080",
+                "lastResetMin": "27116834",
+                "nonce": "1"
+            },
+            {
+                "delegate": "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B",
+                "token": "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+                "amount": "1000000000000000000",
+                "spent": "500000000000000000",
+                "resetTimeMin": "0",
+                "lastResetMin": "26926257",
+                "nonce": "3"
+            },
+            {
+                "delegate": "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B",
+                "token": "0xa7D1C04fAF998F9161fC9F800a99A809b84cfc9D",
+                "amount": "1000000000000000000",
+                "spent": "0",
+                "resetTimeMin": "0",
+                "lastResetMin": "27067549",
+                "nonce": "1"
+            },
+            {
+                "delegate": "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+                "token": "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+                "amount": "1000000000000000000",
+                "spent": "420000000000000000",
+                "resetTimeMin": "0",
+                "lastResetMin": "26772123",
+                "nonce": "3"
+            },
+            {
+                "delegate": "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+                "token": "0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99",
+                "amount": "1000000000000000000",
+                "spent": "0",
+                "resetTimeMin": "10080",
+                "lastResetMin": "27117826",
+                "nonce": "1"
+            },
+            {
+                "delegate": "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+                "token": "0x577D296678535e4903D59A4C929B718e1D575e0A",
+                "amount": "100000000",
+                "spent": "0",
+                "resetTimeMin": "0",
+                "lastResetMin": "27067551",
+                "nonce": "1"
+            },
+            {
+                "delegate": "0x7724b234c9099C205F03b458944942bcEBA13408",
+                "token": "0x35f63A9B5E5bF8ee3ec40317f977809314053104",
+                "amount": "1500000000000000000",
+                "spent": "0",
+                "resetTimeMin": "1440",
+                "lastResetMin": "27124789",
+                "nonce": "4"
+            },
+            {
+                "delegate": "0x7724b234c9099C205F03b458944942bcEBA13408",
+                "token": "0x0000000000000000000000000000000000000000",
+                "amount": "1000000000000000000",
+                "spent": "0",
+                "resetTimeMin": "43200",
+                "lastResetMin": "27111265",
+                "nonce": "7"
+            }
+        ],
+        "balances": [
+            {
+                "tokenAddress": "0x9C5AE6852622ddE455B6Fca4C1551FC0352531a3",
+                "fiatBalance": "463090.9254",
+                "tokenBalance": "9748.297891954973412079"
+            },
+            {
+                "tokenAddress": "0x0000000000000000000000000000000000000000",
+                "fiatBalance": "38667.3958",
+                "tokenBalance": "16.659799999999892"
+            },
+            {
+                "tokenAddress": "0x577D296678535e4903D59A4C929B718e1D575e0A",
+                "fiatBalance": "675.2276",
+                "tokenBalance": "26.8"
+            },
+            {
+                "tokenAddress": "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+                "fiatBalance": "250.2038",
+                "tokenBalance": "0.10780000000000026"
+            },
+            {
+                "tokenAddress": "0xd6801a1DfFCd0a410336Ef88DeF4320D6DF1883e",
+                "fiatBalance": "192.1475",
+                "tokenBalance": "13.682308"
+            },
+            {
+                "tokenAddress": "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+                "fiatBalance": "183.4177",
+                "tokenBalance": "26.39455626550099999"
+            },
+            {
+                "tokenAddress": "0xa7D1C04fAF998F9161fC9F800a99A809b84cfc9D",
+                "fiatBalance": "1.7435",
+                "tokenBalance": "2"
+            },
+            {
+                "tokenAddress": "0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b",
+                "fiatBalance": "0.9908",
+                "tokenBalance": "13.843933"
+            },
+            {
+                "tokenAddress": "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02",
+                "fiatBalance": "0.0003",
+                "tokenBalance": "1.1"
+            },
+            {
+                "tokenAddress": "0xddea378A6dDC8AfeC82C36E9b0078826bf9e68B6",
+                "fiatBalance": "0",
+                "tokenBalance": "0.000010001"
+            },
+            {
+                "tokenAddress": "0x52201ff1720134bBbBB2f6BC97Bf3715490EC19B",
+                "fiatBalance": "0",
+                "tokenBalance": "49.23291797"
+            },
+            {
+                "tokenAddress": "0xEBe09eB3411D18F4FF8D859e096C533CAC5c6B60",
+                "fiatBalance": "0",
+                "tokenBalance": "58.70701711"
+            },
+            {
+                "tokenAddress": "0xEBf1A11532b93a529b5bC942B4bAA98647913002",
+                "fiatBalance": "0",
+                "tokenBalance": "48.55220526"
+            },
+            {
+                "tokenAddress": "0x6D7F0754FFeb405d23C51CE938289d4835bE3b14",
+                "fiatBalance": "0",
+                "tokenBalance": "125.91407043"
+            },
+            {
+                "tokenAddress": "0x5B281A6DdA0B271e91ae35DE655Ad301C976edb1",
+                "fiatBalance": "0",
+                "tokenBalance": "280.25173125"
+            },
+            {
+                "tokenAddress": "0x0014F450B8Ae7708593F4A46F8fa6E5D50620F96",
+                "fiatBalance": "0",
+                "tokenBalance": "38.74843067"
+            },
+            {
+                "tokenAddress": "0xFab46E002BbF0b4509813474841E0716E6730136",
+                "fiatBalance": "0",
+                "tokenBalance": "0.0000000001"
+            },
+            {
+                "tokenAddress": "0x9Ea007843318B9EcD85f93eCC55D4e19143f007A",
+                "fiatBalance": "0",
+                "tokenBalance": "2.586959575158351494"
+            },
+            {
+                "tokenAddress": "0x029B4Ec0e1A01BCEfFb64591b23315b1D81bd82A",
+                "fiatBalance": "0",
+                "tokenBalance": "129937.32"
+            },
+            {
+                "tokenAddress": "0xcBBb17D9767bD57FBF4Bbf8842E916bCb3826ec1",
+                "fiatBalance": "0",
+                "tokenBalance": "48.99"
+            },
+            {
+                "tokenAddress": "0x1150FcF21c5fb154e971fb526A0A777907F87579",
+                "fiatBalance": "0",
+                "tokenBalance": "3.61"
+            },
+            {
+                "tokenAddress": "0x35f63A9B5E5bF8ee3ec40317f977809314053104",
+                "fiatBalance": "0",
+                "tokenBalance": "48.1"
+            },
+            {
+                "tokenAddress": "0xfEbE5Fe18516b57f42dcE2F90c4BFcf6d90033b8",
+                "fiatBalance": "0",
+                "tokenBalance": "1"
+            },
+            {
+                "tokenAddress": "0xB4D0593CEC06D78dB3723Ae442F6FE2662103928",
+                "fiatBalance": "0",
+                "tokenBalance": "0.27901"
+            },
+            {
+                "tokenAddress": "0xeD9Cf84Fa34d292475EA6289c35707a04D325351",
+                "fiatBalance": "0",
+                "tokenBalance": "1.647"
+            },
+            {
+                "tokenAddress": "0x42ed6e98186Bc9C3584Fb19ff0eDC193Dfd4ec38",
+                "fiatBalance": "0",
+                "tokenBalance": "6"
+            },
+            {
+                "tokenAddress": "0x976Eed9d68b21544c61Be7a2A8c80Cde4AA5BE11",
+                "fiatBalance": "0",
+                "tokenBalance": "4.95"
+            }
+        ],
+        "nonce": 1135,
+        "currentVersion": "1.1.1",
+        "needsUpdate": false,
+        "featuresEnabled": [
+            "ERC721",
+            "ERC1155",
+            "SAFE_APPS",
+            "CONTRACT_INTERACTION"
+        ],
+        "loadedViaUrl": false
+    },
+    "0x612DcD86cfa8AeA16C3B3f9E204694B51466F0F6":
+    {
+        "address": "0x612DcD86cfa8AeA16C3B3f9E204694B51466F0F6",
+        "threshold": 1,
+        "ethBalance": "0",
+        "totalFiatBalance": "0.00",
+        "owners": [
+            "0xF2CeA96575d6b10f51d9aF3b10e3e4E5738aa6bd",
+            "0x7724b234c9099C205F03b458944942bcEBA13408",
+            "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+            "0x6484a2514AEE516DdaC6f67Dd2322f23e0A4A7D6"
+        ],
+        "modules": null,
+        "spendingLimits": [],
+        "balances": [
+            {
+                "tokenAddress": "0x0000000000000000000000000000000000000000",
+                "fiatBalance": "0",
+                "tokenBalance": "0"
+            }
+        ],
+        "nonce": 5,
+        "currentVersion": "1.3.0",
+        "needsUpdate": false,
+        "featuresEnabled": [
+            "ERC721",
+            "ERC1155",
+            "SAFE_APPS",
+            "CONTRACT_INTERACTION"
+        ],
+        "loadedViaUrl": false
+    },
+    "0x5BC79B27731589B43c51f745315ca899b4056f33":
+    {
+        "address": "0x5BC79B27731589B43c51f745315ca899b4056f33",
+        "threshold": 2,
+        "ethBalance": "25.92",
+        "totalFiatBalance": "66699.77",
+        "owners": [
+            "0x730F87dA2A3C6721e2196DFB990759e9bdfc5083",
+            "0x61a0c717d18232711bC788F19C9Cd56a43cc8872",
+            "0x7724b234c9099C205F03b458944942bcEBA13408",
+            "0x6E45d69a383CECa3d54688e833Bd0e1388747e6B"
+        ],
+        "modules": null,
+        "spendingLimits": [],
+        "balances": [
+            {
+                "tokenAddress": "0x0000000000000000000000000000000000000000",
+                "fiatBalance": "66699.774",
+                "tokenBalance": "25.92"
+            }
+        ],
+        "nonce": 670,
+        "currentVersion": "1.1.1",
+        "needsUpdate": false,
+        "featuresEnabled": [
+            "ERC721",
+            "ERC1155",
+            "SAFE_APPS",
+            "CONTRACT_INTERACTION"
+        ],
+        "loadedViaUrl": false
+    },
+    "0x00Be14E33760FCD61e46C619eC089a3BfB6d1352": 
+    {
+        "address": "0x00Be14E33760FCD61e46C619eC089a3BfB6d1352",
+        "threshold": 2,
+        "ethBalance": "5",
+        "totalFiatBalance": "10800.00",
+        "owners": [
+            "0x4BaE4C49f02d29F14417bc9bd3237E90cf3e1Efd",
+            "0xac338C3B3A9F574B1D63fb88bf2Fbb17E0Aa3fDf",
+            "0xc293f6eAE3E9170766C74f0867cb4Ad2c8d8fa0c"
+        ],
+        "modules": null,
+        "spendingLimits": [],
+        "balances": [
+            {
+                "tokenAddress": "0x0000000000000000000000000000000000000000",
+                "fiatBalance": "10800",
+                "tokenBalance": "5"
+            }
+        ],
+        "nonce": 0,
+        "currentVersion": "1.1.1",
+        "needsUpdate": false,
+        "featuresEnabled": [
+            "ERC721",
+            "ERC1155",
+            "SAFE_APPS",
+            "CONTRACT_INTERACTION"
+        ],
+        "loadedViaUrl": false
+    }
+}
+
+export const APPS_KEY = "_immortal|v2_RINKEBY__APPS_STORAGE_KEY"
+export const APPS_VALUES = [
+    {
+        "url": "https://jpalvarezl.github.io/eth-wrapper/",
+        "disabled": false
+    }, 
+    {
+        "url": "https://rimeissner.dev/sapp-recorder/",
+        "disabled": false
+    }
+]
