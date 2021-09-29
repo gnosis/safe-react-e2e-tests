c02633cef037af734e2576f40d19eb7dbb473ffe
Diogo Soares <diogo.soares@gnosis.pm>
2021-09-29 17:22:11 +0200

    Create a new helper function to assert that an element is not present

--- utils/selectorsHelpers.js
+++ utils/selectorsHelpers.js
@@ -1,10 +1,10 @@
-const elementSelector = async (selector, page, type, timeout) => {
+const elementSelector = async (selector, page, type, timeout, hidden = false) => {
   /* handling Xpath and css selectors is different. Since many functions require
   to make this distinction this function was created to do it */
   if (type === 'Xpath') {
-    return await page.waitForXPath(selector, { timeout })
+    return await page.waitForXPath(selector, { timeout, hidden })
   } else {
-    return await page.waitForSelector(selector, { timeout })
+    return await page.waitForSelector(selector, { timeout, hidden })
   }
 }
 
@@ -16,7 +16,7 @@ export const clickElement = async function ({ selector, type = 'css' }, page) {
 
 export const clickSomething = async function (selector, page, type = 'Xpath') {
   const element = await elementSelector(selector, page, type, 20000)
-  await page.evaluate(x => x.click(), element)
+  await page.evaluate((x) => x.click(), element)
   return element
 }
 
@@ -41,48 +41,62 @@ export const clearInput = async function ({ selector, type = 'Xpath' }, page) {
   page.keyboard.press('Backspace')
 }
 
-export const assertElementPresent = async function ({selector, type = 'Xpath'}, page) {
+export const assertElementPresent = async function ({ selector, type = 'Xpath' }, page) {
   const element = await elementSelector(selector, page, type, 90000)
   const expectHandler = expect(element)
   type !== 'Xpath' ? expectHandler.not.toBeNull() : expectHandler.toBeDefined() // for Css there is a different condition to make
   return element
 }
 
-export const assertTextPresent = async function ({selector, type = 'Xpath'}, textPresent, page) {
-  const element = await assertElementPresent({selector, type}, page)
-  const elementText = await page.evaluate(x => x.innerText, element)
+export const assertElementNotPresent = async function ({ selector, type = 'Xpath' }, page) {
+  const element = await elementSelector(selector, page, type, 3000, true)
+  expect(element).toBeNull()
+  return element
+}
+
+export const assertTextPresent = async function ({ selector, type = 'Xpath' }, textPresent, page) {
+  const element = await assertElementPresent({ selector, type }, page)
+  const elementText = await page.evaluate((x) => x.innerText, element)
   expect(elementText).toMatch(textPresent)
 }
 
-export const getInnerText = async function ({selector, type = 'Xpath'}, page) {
-  const element = await assertElementPresent({selector, type}, page)
-  let elementText = await page.evaluate(x => x.innerText, element)
+export const getInnerText = async function ({ selector, type = 'Xpath' }, page) {
+  const element = await assertElementPresent({ selector, type }, page)
+  let elementText = await page.evaluate((x) => x.innerText, element)
   if (elementText === '') {
-    elementText = await page.evaluate(x => x.value, element)
+    elementText = await page.evaluate((x) => x.value, element)
   }
   return elementText
 }
 
-export const getNumberInString = async function ({selector, type = 'Xpath'}, page) {
-  const text = await getInnerText({selector, type}, page)
+export const getNumberInString = async function ({ selector, type = 'Xpath' }, page) {
+  const text = await getInnerText({ selector, type }, page)
   const number = text.match(/\d+.?\d+|\d+/)[0]
   return parseFloat(number)
 }
 
 export const clickByText = async function (tag, text, page) {
-  await page.$$eval(tag, (nodes, text) => {
-    console.log('Found nodes: ', nodes)
-    if (nodes.length > 0) {
-      nodes.forEach(singleNode => { if (singleNode.innerText.toLocaleLowerCase() === text.toLocaleLowerCase()) singleNode.click() })
-    } else {
-      console.log('No nodes found')
-    }
-  }
-  , text)
+  await page.$$eval(
+    tag,
+    (nodes, text) => {
+      console.log('Found nodes: ', nodes)
+      if (nodes.length > 0) {
+        nodes.forEach((singleNode) => {
+          if (singleNode.innerText.toLocaleLowerCase() === text.toLocaleLowerCase()) singleNode.click()
+        })
+      } else {
+        console.log('No nodes found')
+      }
+    },
+    text,
+  )
 }
 
 export const isTextPresent = async function (selector, text, page) {
   await page.waitForFunction(
-    (selector, text) => document.querySelector(selector).innerText.includes(text), { timeout: 60000 }, selector, text
+    (selector, text) => document.querySelector(selector).innerText.includes(text),
+    { timeout: 60000 },
+    selector,
+    text,
   )
 }
